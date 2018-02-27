0a08efceb2a6387149e79b44f03b59569850cee6
Julian <julian.gorfer@yahoo.com>
2018-02-27 11:00:02 +0100

    implementation of a very basic environment without obstacles

--- src/Maze.java
+++ src/Maze.java
@@ -1,9 +1,18 @@
 
 public class Maze {
-	//contains the coordinate system, including the walls
-	//also takes care about displaying the maze and robot movement
+    private double maxX;
+    private double maxY;
+
+    /**
+     * contains the coordinate system, including the walls
+     * also takes care about displaying the maze and robot movement
+     */
+    public Maze() {
+        maxX = 30.0;
+        maxY = 30.0;
+    }
 	public boolean updatePosition(double x, double y) {
-		
-	}
+        return !(x < 0) && !(x > maxX) && !(y < 0) && !(y > maxY);
+    }
 
 }
