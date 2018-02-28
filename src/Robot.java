8f820270ab65259a69aadc2861711300d2cb2334
BonkeR93 <33482451+BonkeR93@users.noreply.github.com>
2018-02-28 19:48:40 +0100

    state of the art robot

--- src/Robot.java
+++ src/Robot.java
@@ -7,8 +7,29 @@ public class Robot {
 	Maze maze; 
 	
 	public void move() {
+		//gets information from the NN Vr and Vl
+		double Vr = 1;
+		double Vl = 2;
 		double x=0;
 		double y=0;
+		double theta = 0.2;
+		double deltat = 0.1;
+		double w;
+		double R;
+		double ICCx;
+		double ICCy;
+		if (Vr == Vl) {
+				//just move forward;
+		}
+		else {
+			R = (l/2)*((Vl+Vr)/(Vr-Vl));
+			w = (Vr - Vl) / l;
+			ICCx = x - R* Math.sin(theta);
+			ICCy = y + R* Math.cos(theta);
+			
+		}
+			
+		
 		maze.updatePosition(x,y);
 	}
 	
