e6dd99b00f788607be6525e3600d3157f083dfbd
Timo Raff <timoraff@gmail.com>
2018-03-20 14:27:44 +0100

    reduced to neccessary classes

--- src/Main.java
+++ src/Main.java
@@ -25,74 +25,15 @@ public class Main {
 
 	// just a main to execute the programm
 	public static void main(String[] args) {
-		/*
-		 * init a new population with size STARTINGPOP evolve this population for a
-		 * number of "ITERATIONS" iterations
-		 */
-		if (!JUSTLOAD && !CONTINUETRAINING) {
-			Population pop = new Population(STARTINGPOP);
-			for (int i = 0; i < ITERATIONS; i++) {
-				System.out.println("Evo No: " + i);
-				pop = Controller.evolveEvolution(pop);
-			}
-
-			try {
-				FileOutputStream fileOut = new FileOutputStream("/tmp/population.ser");
-				ObjectOutputStream out = new ObjectOutputStream(fileOut);
-				out.writeObject(pop);
-				out.close();
-				fileOut.close();
-				System.out.printf("Serialized data is saved in /tmp/population.ser");
-			} catch (IOException i) {
-				i.printStackTrace();
-			}
-		}
-		Population p;
-		try {
-			FileInputStream fileIn = new FileInputStream("/tmp/population.ser");
-			ObjectInputStream in = new ObjectInputStream(fileIn);
-			p = (Population) in.readObject();
-			in.close();
-			fileIn.close();
-		} catch (IOException i) {
-			i.printStackTrace();
-			return;
-		} catch (ClassNotFoundException c) {
-			System.out.println("population class not found");
-			c.printStackTrace();
-			return;
-		}
-
-		if (CONTINUETRAINING && !JUSTLOAD) {
-			for (int i = 0; i < ITERATIONS; i++) {
-				System.out.println("Evo No: " + i);
-				p = Controller.evolveEvolution(p);
-			}
-
-			try {
-				FileOutputStream fileOut = new FileOutputStream("/tmp/population.ser");
-				ObjectOutputStream out = new ObjectOutputStream(fileOut);
-				out.writeObject(p);
-				out.close();
-				fileOut.close();
-				System.out.printf("Serialized data is saved in /tmp/population.ser");
-			} catch (IOException i) {
-				i.printStackTrace();
-			}
-		}
-
-		// how to get the correct position for the robot
 		Maze maze = new Maze();
 		Robot robo = new Robot(STARTINGX, STARTINGY, maze);
 		Visualizer visualizer = new Visualizer(maze, robo);
 		robo.setVisualizer(visualizer);
 
 		// let the fittest model play for some time --> for seeing a result..
-		NeuralNet fittest = p.getFittest();
 		for (int i = 0; i < 200000; i++) {
-			double[] tmp = fittest.calculate(robo.getSensorValues());
-			System.out.println("Move: " + Arrays.toString(tmp));
-			robo.move(tmp/* fittest.calculate(robo.getSensorValues()) */);
+			double []tmp= new double[] {1,2,3};
+			robo.move(tmp);
 			try {
 				Thread.sleep(18);
 				visualizer.update();
--- src/SelectionStrategy.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- * @author Timo Raff
- * selectino strategy interface for the selection of individuals for the EA
- */
-public interface SelectionStrategy {
-	
-	public NeuralNet select(Population pop);
-}
